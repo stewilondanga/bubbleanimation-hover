0e36022f209e2e7b0f092fe4d48cfa8129c7b7ee
Stewart ilondanga <stilondanga@gmail.com>
2018-11-24 18:34:31 +0300

    add js/scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -7,25 +7,25 @@ var navigate = (function() {
   });
 })();
 
-/*$( ".button_su_inner" ).mouseenter(function(e) {
-   var parentOffset = $(this).offset();
+$(".button_su_inner").mouseenter(function(e) {
+      var parentOffset = $(this).offset();
 
-   var relX = e.pageX - parentOffset.left;
-   var relY = e.pageY - parentOffset.top;
-   $(this).prev(".su_button_circle").css({"left": relX, "top": relY });
-   $(this).prev(".su_button_circle").removeClass("desplode-circle");
-   $(this).prev(".su_button_circle").addClass("explode-circle");
+      /*   var relX = e.pageX - parentOffset.left;
+         var relY = e.pageY - parentOffset.top;
+         $(this).prev(".su_button_circle").css({"left": relX, "top": relY });
+         $(this).prev(".su_button_circle").removeClass("desplode-circle");
+         $(this).prev(".su_button_circle").addClass("explode-circle");
 
-});
+      });
 
-$( ".button_su_inner" ).mouseleave(function(e) {
+      $( ".button_su_inner" ).mouseleave(function(e) {
 
-     var parentOffset = $(this).offset();
+           var parentOffset = $(this).offset();
 
-     var relX = e.pageX - parentOffset.left;
-     var relY = e.pageY - parentOffset.top;
-     $(this).prev(".su_button_circle").css({"left": relX, "top": relY });
-     $(this).prev(".su_button_circle").removeClass("explode-circle");
-     $(this).prev(".su_button_circle").addClass("desplode-circle");
+           var relX = e.pageX - parentOffset.left;
+           var relY = e.pageY - parentOffset.top;
+           $(this).prev(".su_button_circle").css({"left": relX, "top": relY });
+           $(this).prev(".su_button_circle").removeClass("explode-circle");
+           $(this).prev(".su_button_circle").addClass("desplode-circle");
 
-});
+      });
