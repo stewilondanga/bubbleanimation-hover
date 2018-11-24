e5edc56498372ae8502c706a0f25a3302ba8ce90
Stewart ilondanga <stilondanga@gmail.com>
2018-11-24 18:35:46 +0300

    add js/scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -21,14 +21,17 @@ $(".button_su_inner").mouseenter(function(e) {
 
 });
 
-/*      $( ".button_su_inner" ).mouseleave(function(e) {
+$(".button_su_inner").mouseleave(function(e) {
 
-           var parentOffset = $(this).offset();
+  var parentOffset = $(this).offset();
 
-           var relX = e.pageX - parentOffset.left;
-           var relY = e.pageY - parentOffset.top;
-           $(this).prev(".su_button_circle").css({"left": relX, "top": relY });
-           $(this).prev(".su_button_circle").removeClass("explode-circle");
-           $(this).prev(".su_button_circle").addClass("desplode-circle");
+  var relX = e.pageX - parentOffset.left;
+  var relY = e.pageY - parentOffset.top;
+  $(this).prev(".su_button_circle").css({
+    "left": relX,
+    "top": relY
+  });
+  $(this).prev(".su_button_circle").removeClass("explode-circle");
+  $(this).prev(".su_button_circle").addClass("desplode-circle");
 
-      });
+});
