bb074d1266057d70f0981932a93405785d080be2
Stewart ilondanga <stilondanga@gmail.com>
2018-11-24 18:33:29 +0300

    add js/scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-var navigate = (function() {
+/*var navigate = (function() {
 	$('.dd').toggle();
 	$('.dd_btn').click(function() {
 		var dataName = $(this).attr('data-name');
@@ -6,3 +6,26 @@ var navigate = (function() {
 		$('.' + dataName).toggle();
 	});
 })();
+
+$( ".button_su_inner" ).mouseenter(function(e) {
+   var parentOffset = $(this).offset();
+
+   var relX = e.pageX - parentOffset.left;
+   var relY = e.pageY - parentOffset.top;
+   $(this).prev(".su_button_circle").css({"left": relX, "top": relY });
+   $(this).prev(".su_button_circle").removeClass("desplode-circle");
+   $(this).prev(".su_button_circle").addClass("explode-circle");
+
+});
+
+$( ".button_su_inner" ).mouseleave(function(e) {
+
+     var parentOffset = $(this).offset();
+
+     var relX = e.pageX - parentOffset.left;
+     var relY = e.pageY - parentOffset.top;
+     $(this).prev(".su_button_circle").css({"left": relX, "top": relY });
+     $(this).prev(".su_button_circle").removeClass("explode-circle");
+     $(this).prev(".su_button_circle").addClass("desplode-circle");
+
+});
